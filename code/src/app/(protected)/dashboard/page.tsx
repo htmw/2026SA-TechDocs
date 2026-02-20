d500fbbf95ce5f26340b0c8dc239c06b70c647a6
Alan <8147825+Kingal1337@users.noreply.github.com>
2026-02-20 16:47:43 -0500

    add dashboard page

---
+++ code/src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Dashboard",
+};
+
+export default function DashboardPage() {
+    return (<>
+        <div className="max-w-7xl mx-auto px-4">
+            <h1 className="text-3xl font-bold">Dashboard</h1>
+        </div>
+    </>);
+}
