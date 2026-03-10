e5cd18d1405fe054da75ff409668fec15c2ed353
Alan <8147825+Kingal1337@users.noreply.github.com>
2026-03-10 17:15:47 -0400

    feat: add goals and chat page

---
+++ code/src/app/(protected)/(setup_complete)/chat/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Chat',
+};
+
+export default async function Layout({ children }: { children: any }) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
