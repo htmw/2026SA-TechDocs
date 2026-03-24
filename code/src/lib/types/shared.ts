ffef5ed2b3c7a6dafd45229bc82a344b323d984f
Alan <8147825+Kingal1337@users.noreply.github.com>
2026-03-23 23:05:16 -0400

    feat: add jsdoc

--- code/src/lib/types/shared.ts
+++ code/src/lib/types/shared.ts
@@ -1,6 +1,10 @@
 import { ClientSession } from "@/lib/types/mongo_session_types"
 import { ClientUser } from "@/lib/types/mongo_user_types"
 
+/**
+ * A standard API error type that can be used for all API endpoints. 
+ * It includes a code, message, and optional fields for detailed error information.
+ */
 export interface ApiError {
     code: string
     message: string
@@ -13,12 +17,23 @@ export interface ApiError {
     >
 }
 
+/**
+ *  A standard API response type that can be used for all API endpoints.
+ *  It includes a success boolean, an optional data field of type T, and an optional error field of type ApiError.
+ */
 export interface ApiResponse<T> {
     success: boolean
     data?: T
     error?: ApiError
 }
 
+/**
+ * Creates an error response for API calls.
+ * @param code A string code representing the error type, e.g. "VALIDATION_ERROR", "NOT_FOUND", etc.
+ * @param message The error message.
+ * @param fields Optional fields for detailed error information.
+ * @returns The error response object.
+ */
 export function createErrorResponse(code: string, message: string, fields?: Record<string, string | string[] | Record<string, string | Record<string, string> | string[] | undefined> | undefined>): ApiResponse<null> {
     return {
         success: false,
@@ -30,6 +45,11 @@ export function createErrorResponse(code: string, message: string, fields?: Reco
     }
 }
 
+/**
+ * Creates a success response for API calls.
+ * @param data The data to include in the success response.
+ * @returns The success response object.
+ */
 export function createSuccessResponse<T>(data: T): ApiResponse<T> {
     return {
         success: true,
@@ -37,6 +57,10 @@ export function createSuccessResponse<T>(data: T): ApiResponse<T> {
     }
 }
 
+/**
+ * Type representing the authentication state of the user.
+ * It can either be an authenticated state with a session and user, or an unauthenticated state with null values.
+ */
 export type AuthState = {
     session: ClientSession;
     user: ClientUser;
