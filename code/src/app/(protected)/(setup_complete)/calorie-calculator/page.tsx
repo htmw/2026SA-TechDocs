0c2f08db6664fb0a27047a68c9b79fe321b56e99
Alan <8147825+Kingal1337@users.noreply.github.com>
2026-04-02 19:05:00 -0400

    feat: add hooks and examples to calorie calculator page

--- code/src/app/(protected)/(setup_complete)/calorie-calculator/page.tsx
+++ code/src/app/(protected)/(setup_complete)/calorie-calculator/page.tsx
@@ -1,6 +1,99 @@
+"use client";
+
+import { SingleWeekPicker } from "@/components/cards/single_week_picker";
+import { useDailyLogStatus } from "@/lib/hooks/api-hooks/use-daily-log";
+import { useFoods } from "@/lib/hooks/api-hooks/use-food";
+import { useCreateMeal, useDeleteMeal, useMeals } from "@/lib/hooks/api-hooks/use-meals";
+import { endOfWeek, startOfWeek } from "date-fns";
+import React from "react";
+
 export default function CalorieCalculatorPage() {
+    // API hook to create a meal
+    /**
+     * Example request body
+     * {
+     *   "meal_type": "breakfast",
+     *   "food_item": "Scrambled Eggs",
+     *   "calories": 180,
+     *   "protein": 12,
+     *   "carbohydrates": 2,
+     *   "fat": 14,
+     *   "fiber": 0,
+     *   "sugar": 1,
+     *   "sodium": 180,
+     *   "cholesterol": 370,
+     *   "water_intake": 250,
+     *   "serving_quantity": 2,
+     *   "servings": 3,
+     *   "logged_at": "2026-03-28T21:40:24.724Z"
+     * }
+     * 
+     * create_meal.mutate(body);
+     */
+    const create_meal = useCreateMeal();
+
+    // States for week picker
+    const [selected_date, setSelectedDate] = React.useState(new Date());
+    const [week_start, setWeekStart] = React.useState(startOfWeek(selected_date, { weekStartsOn: 0 }));
+    const [week_end, setWeekEnd] = React.useState(endOfWeek(selected_date, { weekStartsOn: 0 }));
+
+    // Used to get status for a specific day in the week picker
+    const { data: day_status_data = [], isLoading: loading_day_statuses } = useDailyLogStatus({
+        startDate: week_start,
+        endDate: week_end,
+        status: "meals",
+    });
+    const day_status_array = day_status_data.map(status => {
+        if (status.meals && status.meals > 0) {
+            return status.date
+        }
+    }).filter(status => status !== undefined);
+
+    const { data: meals = [], isLoading: meals_loading } = useMeals(selected_date);
+
+    /**
+     * Usage Example:
+     * delete_meal.mutate({ date, meal_id }, { 
+     *      onSuccess: () => console.log("Meal deleted successfully"), // refresh meals or remove the meal from local state
+     *      onError: (err) => console.error("Failed to delete meal", err) 
+     * })
+     */
+    const delete_meal = useDeleteMeal();
+
+    // Example usage of useFoods hook with various parameters
+    // Uncomment paramters as needed
+    // The current example get the lowest calorie steak with at least 30g of protein
+    const { data: foods = [] } = useFoods({
+        limit: 5,
+        sort_field: "calories",
+        sort_dir: "asc",
+        food_item_contains : "Steak",
+        // category : "Grain", //custom field
+        // calories_lt : 500,
+        // sodium_lt: 100 // doesn't exist in UseFoodsParams, but you can add custom parameters if needed
+        protein_gte: 30
+    });
 
     return (
-        <></>
+        <>
+            <div className="gap-5 p-6 grid grid-cols-1">
+                <SingleWeekPicker
+                    value={selected_date}
+                    onChange={(date, start_week, end_week) => {
+                        setSelectedDate(date);
+                        setWeekStart(start_week);
+                        setWeekEnd(end_week);
+                    }}
+                    weekStartsOn={0}
+                    day_statuses={day_status_array}
+                />
+
+                <h1>Meals</h1>
+                <pre>{JSON.stringify(meals, null, 2)}</pre>
+                ---
+                <h1>Foods</h1>
+                <pre>{JSON.stringify(foods, null, 2)}</pre>
+            </div>
+        </>
     );
 }
